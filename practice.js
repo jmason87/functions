ba3bb8608bd87f74ae7c6fc974fe05fd1c78e03f
jmason87 <joseph.mason87@gmail.com>
2021-10-18 11:15:28 -0500

    initial commit

---
+++ practice.js
@@ -0,0 +1,133 @@
+// Dog Years
+// const calculateAgeInDogYears = (ageOfPerson) => {
+//     const ageInDogYears = ageOfPerson / 7
+    
+//     return ageInDogYears
+// }
+
+// const dogAge = calculateAgeInDogYears(45)
+
+// console.log(dogAge)
+
+
+//Best In Show
+
+// const favoriteDogBreed = (dogBreed) => {2
+//     if (dogBreed === "schnauzer") {
+//         return "My favorite dog breed is schnauzer"
+//     } else if (dogBreed === "meow") {
+//         return "I like Cats"
+//     }
+// }
+
+// const myFavorite = favoriteDogBreed()
+
+// console.log(`When it comes to pets, ${myFavorite}`)
+
+
+// Addition
+
+// const add = (num1, num2, num3) => {
+//     return num1 + num2 + num3
+// }
+
+// const functionSum = add(2, 2, 3)
+
+
+// console.log(functionSum)
+
+
+// Self-Driving Cars
+
+// const go = (direction, speed) => {
+//     if(speed > 75) {
+//         console.log(`The car is moving ${direction} at ${speed} mph. SLOW DOWN!`)
+//     } else {
+//         console.log(`The car is moving ${direction} at ${speed} mph`)
+//     }
+// }
+
+// go("forwards", 83)
+
+// =======================================================================
+// Even or Odd
+
+// const evenOrOdd = (num) => {
+//     const remainderNum = num % 2
+//     if(remainderNum === 0 ) {
+//         return "Even"
+//     } else {
+//         return "odd"
+//     }
+// }
+// const numResult = evenOrOdd(4)
+// console.log(numResult)
+
+// const numArray = [3, 7, 8, 2, 14, 9]
+
+// for (const numArrayObj of numArray) {
+//     evenOrOdd()
+// }
+// console.log(numArrayObj)
+// ==========================================================================
+
+
+//Double Functions
+
+// const words = [
+//     "The", "killing", "complex", "houses",
+//     "married", "kittens", "and", "single",
+//     "soldiers", "and", "their", "kleptomaniacal",
+//     "families"]
+    
+//     const kFilter = () => {
+//         const newArray = []
+//         for (const filterObj of words) {
+//             if (filterObj.startsWith("k") === false) {
+//                 newArray.push(filterObj)
+//             }
+//         }
+//       return newArray.join(" ")  
+//     }
+    
+    
+//     console.log(kFilter())
+
+    // console.log(words.join(" "))
+
+
+
+//Svens Fish
+
+// const svensFish = () => {
+//     const svensChances = Math.random() * 3
+//     if (svensChances < 2) {
+//         return "Sven came up empty-handed. :("
+//     } else {
+//         return "Sven hooked a tuna!"
+//     }
+// }   
+
+// console.log(svensFish())
+
+
+// Food Order
+
+// const foodOrder = (sand, side, drink, dessert) => {
+//     return `"sandwich: ${sand},
+//             "side": ${side},
+//             "drink: ${drink},
+//             "dessert": ${dessert}`
+// }
+
+// console.log(foodOrder("Big Mac", "Fries", "Coke", "Vanilla Shake"))
+
+
+// const chore1 = (chore) => {
+//     const name = {
+//         firstName: "Donald",
+//         lastName: "McLelland"
+//     }
+        
+// }
+    
